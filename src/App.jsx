cbd55de29e97ea1d16a42c2e04dd97cde2f7266c
Monika <monikachand714@gmail.com>
2023-11-14 12:01:34 +0530

    create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-red-500'>Hello</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
 
-export default App
+export default App;
