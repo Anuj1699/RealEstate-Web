77412b704df32759490f9dc26b2eeb8601633f74
Monika <monikachand714@gmail.com>
2023-11-15 14:31:32 +0530

    signup UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -31,7 +31,7 @@ export default function Header() {
                 About
               </li>
             </Link>
-            <Link to={"/sign-in"}>
+            <Link to={"/sign-up"}>
               <li className="text-slate-700 hover:underline cursor-pointer">
                 SignIn
               </li>
