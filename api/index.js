43d4883053ee08c902971d4f1e2cfc141b319d7d
Monika <monikachand714@gmail.com>
2023-11-15 10:58:13 +0530

    add database

--- api/index.js
+++ api/index.js
@@ -1,18 +1,25 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-mongoose.connect(process.env.MONGO).then(() => console.log("Connection Successfull"))
+// Adjust the path to your .env file based on its location
+const envPath = resolve(__dirname, '../.env');
+dotenv.config({ path: envPath });
+
+mongoose.connect(process.env.MONGO).then(() => console.log("Connection Successful"))
 .catch((err) => console.log(err))
 
 const app = express();
 
-app.get("/",(req,res) =>{
-    res.send("Hello")
-})
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Port 3000 running...");
-})
+});
