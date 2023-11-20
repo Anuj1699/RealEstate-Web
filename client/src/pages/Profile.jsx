05abd608bd993e4038adf72de12b84680c38b043
Monika <monikachand714@gmail.com>
2023-11-20 21:56:01 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,22 +1,104 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import { app } from "./../firebase";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 export default function Profile() {
-  const {currentUser} = useSelector((state) => state.user)
+  const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+          setFormData({ ...formData, avatar: downloadUrl });
+        });
+      }
+    );
+  };
+  const fileRef = useRef(null);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
-         <img src={currentUser.avatar} className="rounded-full h-24 w-24 object-contain cursor-pointer my-2 self-center" alt="profile-pic" />
-         <input type="username" placeholder="username" name="username" className="border rounded-lg p-3"/>
-         <input type="email" placeholder="email" name="email" className="border rounded-lg p-3"/>
-         <input type="password" placeholder="password" name="password" className="border rounded-lg p-3"/>
-         <button className=" uppercase bg-slate-700 rounded-lg p-3 text-white hover:opacity-90 disabled:opacity-70">update</button>
+        <input
+          type="file"
+          ref={fileRef}
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+          hidden
+        />
+        <img
+          src={formData.avatar || currentUser.avatar}
+          onClick={() => fileRef.current.click()}
+          className="rounded-full h-24 w-24 object-cover cursor-pointer my-2 self-center"
+          alt="profile-pic"
+        />
+        <p className="text-center">
+          {fileUploadError ? (
+            <span className=" text-red-700">Error Upload Image</span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className=" text-green-700">
+              Image Successfully Uploaded!
+            </span>
+          ) : (
+            ""
+          )}
+        </p>
+        <input
+          type="username"
+          placeholder="username"
+          name="username"
+          className="border rounded-lg p-3"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          className="border rounded-lg p-3"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          className="border rounded-lg p-3"
+        />
+        <button className=" uppercase bg-slate-700 rounded-lg p-3 text-white hover:opacity-90 disabled:opacity-70">
+          update
+        </button>
       </form>
       <div className="flex justify-between mt-3 text-red-600 cursor-pointer">
         <span>Delete Account</span>
         <span>SignOut</span>
       </div>
     </div>
-
-  )
+  );
 }
